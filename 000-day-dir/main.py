e54c2c38380b489441fcc65449a9dc0ae07bfe77
Eric V <ericvanmeter57@gmail.com>
2022-10-11 17:41:06 -0400

    added structure and worked on vars

---
+++ 000-day-dir/main.py
@@ -0,0 +1,6 @@
+
+print("Welcome to the Band Name Generator.")
+city_name = input("What city did you grow up in? ")
+pet_name = input("What is the name of your pet? ")
+band_name = f"{city_name} {pet_name}"
+print(f"Your band name is {band_name}")
